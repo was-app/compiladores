160838f4b389197cab5ff193b538dc42ff365846
Wasterman Apolinário <119600193+was-app@users.noreply.github.com>
2025-04-23 12:22:18 -0300

    scanner implementation

---
+++ scanner.py
@@ -0,0 +1,41 @@
+import re
+
+
+class Scanner:
+  def __init__(self, specification, keywords):
+    self.possible_tokens = specification
+    self.keywords = keywords
+    self.regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in self.possible_tokens)
+    self.get_token = re.compile(self.regex).match
+
+  def tokenize(self, code):
+    tokens = []
+    line = 1
+    position = 0
+    token = self.get_token(code,position)
+
+    while token:
+      type = token.lastgroup
+      value = token.group()
+
+      if type == 'NEWLINE':
+        line += 1
+        pass
+      elif type in ('SKIP', 'COMMENT'):
+        pass
+      elif type == 'ID' and value in self.keywords:
+        tokens.append({'type': 'KEYWORD', 'value': value})
+      elif type == 'MISMATCH':
+        position = token.end()
+        while position < len(code) and code[position] not in ';':
+          position +=1
+        print(f"Erro: [[{value}]], Linha {line}")
+        token = self.get_token(code, position)
+        continue
+      else:
+        tokens.append({'type': type, 'value': value})
+      
+      position = token.end()
+      token = self.get_token(code, position)
+
+    return tokens
---
+++ test.c
@@ -0,0 +1,9 @@
+int main() {
+  int x = 5;
+  int y_4= 10;
+  int z=4;
+  float a = 1e-5;
+  int t = y_4 >= x;
+  char c = 'c;
+  return x + y_4+z;
+}
